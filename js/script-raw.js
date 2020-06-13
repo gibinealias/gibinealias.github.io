f4d6a56435ab825cb3d287f800d93f00d8e1a489
Gibin <gibin.ealias@borngroup.com>
2020-06-13 21:18:01 +0100

    optimizations

---
+++ js/script-raw.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.header .hamburger-icon').on('click', function(){
+		$(this).toggleClass('open');
+		$('.main-nav').toggleClass('open');
+	});
+
+	$('.menu-item.has-sub-menu > .menu-link').on('click', function(e){
+		e.preventDefault();
+		$(this).parent().toggleClass('open');
+	});
+
+	$('#whyUsCarousel').slick({
+		arrows: false,
+	  dots: true,
+	  infinite: true,
+		autoplay: true
+	});
+});
