4923c630e2268e8433b4e57cf07f8c94fee27746
Gibin <gibin.ealias@borngroup.com>
2021-01-14 19:44:47 +0000

    Test11

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 window.onload = function(){ 
-    var iframe = document.getElementById('app-frame');
-    iframe.onload = function() {
-        var iframeW = iframe.contentWindow;
-        iframeW.addEventListener('click', function () {
-            let a = iframeW.pc.app;
-            console.log(a.currentCase +' -- '+ a.currentStrap);
-        });
-    };
+    var iframeW = document.getElementById('app-frame').contentWindow;
+    iframeW.addEventListener('click', function () {
+        let a = iframeW.pc.app;
+        console.log(a.currentCase +' -- '+ a.currentStrap);
+    });
 }
