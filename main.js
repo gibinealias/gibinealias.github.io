6ba8b8966f2b94b15aa61f810d6131e405ee6270
Gibin <gibin.ealias@borngroup.com>
2021-01-14 20:00:16 +0000

    Test2

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-window.onload = function(){ 
-    var iframeW = document.getElementById('app-frame').contentWindow;
-    iframeW.addEventListener('click', function () {
-        let a = iframeW.pc.app;
-        console.log(a.currentCase +' -- '+ a.currentStrap);
+window.onload = function(){
+    let iframeW = document.getElementById('app-frame').contentWindow,
+        app = iframeW.pc.Application.getApplication();
+
+    app.on("update_strap", function (data) {
+        console.log(data);
+    });
+
+    app.on("update_case", function (data) {
+        console.log(data);
     });
 }
