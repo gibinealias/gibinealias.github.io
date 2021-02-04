f6ecb1b318a312e357c183fe2040999b4e60fabd
Gibin <gibin.ealias@borngroup.com>
2021-02-04 00:41:45 +0000

    About us page

--- js/script-raw.js
+++ js/script-raw.js
@@ -4,10 +4,18 @@ $(document).ready(function(){
 		$('.main-nav').toggleClass('open');
 	});
 
-	$('#whyUsCarousel').slick({
+	$('[data-slick]').slick({
 		arrows: false,
 		dots: true,
 		infinite: true,
 		autoplay: true
+	});
+
+	$('.tab-header').on('click', function(){
+		$('.tab-header').removeClass('selected');
+		$(this).addClass('selected');
+		$('.tab-content').addClass('d-none');
+		$('#'+$(this).data('tab')).removeClass('d-none');
+		this.scrollIntoView({behavior: "smooth", block: "nearest"});
 	});
 });
